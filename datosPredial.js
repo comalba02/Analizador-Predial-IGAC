e4978ee62fda374462716e80d069b24d00c812b0
Marco Alberto Coronado Baquero <49756024+comalba02@users.noreply.github.com>
2026-04-05 15:25:53 -0500

    Create datosPredial.js

---
+++ datosPredial.js
@@ -0,0 +1,54 @@
+export const zonas = { "00": "Rural", "01": "Urbana" };
+
+export const condiciones = { 
+    "0": "No propiedad horizontal",
+    "9": "Propiedad horizontal",
+    "8": "Conjunto residencial / Condominio",
+    "7": "Parques y cementerios",
+    "4": "Vías",
+    "3": "Bienes de uso público diferentes a vías",
+    "2": "Informales"
+};
+
+export const departamentos = {
+    "05": "ANTIOQUIA",
+    "08": "ATLÁNTICO",
+    "11": "BOGOTÁ, D.C.",
+    "13": "BOLÍVAR",
+    "15": "BOYACÁ",
+    "17": "CALDAS",
+    "18": "CAQUETÁ",
+    "19": "CAUCA",
+    "20": "CESAR",
+    "23": "CÓRDOBA",
+    "25": "CUNDINAMARCA",
+    "27": "CHOCÓ",
+    "41": "HUILA",
+    "44": "LA GUAJIRA",
+    "47": "MAGDALENA",
+    "50": "META",
+    "52": "NARIÑO",
+    "54": "NORTE DE SANTANDER",
+    "63": "QUINDÍO",
+    "66": "RISARALDA",
+    "68": "SANTANDER",
+    "70": "SUCRE",
+    "73": "TOLIMA",
+    "76": "VALLE DEL CAUCA",
+    "81": "ARAUCA",
+    "85": "CASANARE",
+    "86": "PUTUMAYO",
+    "88": "SAN ANDRÉS",
+    "91": "AMAZONAS",
+    "94": "GUAINÍA",
+    "95": "GUAVIARE",
+    "97": "VAUPÉS",
+    "99": "VICHADA"
+};
+
+export const municipios = {
+    "50001": "Villavicencio",
+    "50006": "Acacías",
+    "50110": "Barranca de Upía"
+    // deja aquí TODOS los municipios
+};
